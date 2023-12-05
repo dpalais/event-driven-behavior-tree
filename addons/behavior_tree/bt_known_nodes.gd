f4c051fde0ea6e9d964a836a7b115e0680723596
Dave Palais <active.hat0408@fastmail.com>
2023-12-05 14:55:46 -0500

    Chain additional known nodes resources

--- addons/behavior_tree/bt_known_nodes.gd
+++ addons/behavior_tree/bt_known_nodes.gd
@@ -1,4 +1,5 @@
 @tool
+class_name BTKnownNodes
 extends Resource
 
 @export var composites: Array
@@ -6,3 +7,30 @@ extends Resource
 @export var leaves: Array
 @export var utilities: Array
 @export var values: Array
+
+@export var additional_nodes: BTKnownNodes
+
+func get_composite_nodes() -> Array:
+	var additional = [] if additional_nodes == null else additional_nodes.composites
+	return _get_nodes(composites, additional)
+
+func get_condition_nodes() -> Array:
+	var additional = [] if additional_nodes == null else additional_nodes.conditions
+	return _get_nodes(composites, additional)
+
+func get_leaf_nodes() -> Array:
+	var additional = [] if additional_nodes == null else additional_nodes.leaves
+	return _get_nodes(leaves, additional)
+
+func get_utility_nodes() -> Array:
+	var additional = [] if additional_nodes == null else additional_nodes.utilities
+	return _get_nodes(utilities, additional)
+
+func get_value_nodes() -> Array:
+	var additional = [] if additional_nodes == null else additional_nodes.values
+	return _get_nodes(values, additional)
+
+func _get_nodes(from: Array, additional: Array = []) -> Array:
+	var ret := from.duplicate()
+	ret.append_array(additional)
+	return ret
